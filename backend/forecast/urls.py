f26db6e376033754fa77c8bd43bc55829fb1285c
Olatilewa Braimah <brymarjr@wale.localdomain>
2026-01-11 22:07:32 +0100

    Fix: Remove pycache and binary files from git tracking

--- backend/forecast/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from forecast.views import ForecastViewSet
-
-router = DefaultRouter()
-router.register('forecasts', ForecastViewSet, basename='forecast')
-
-urlpatterns = router.urls
--- backend/purchases/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import PurchaseOrderViewSet
-
-router = DefaultRouter()
-router.register(r'purchases', PurchaseOrderViewSet, basename='purchase')
-
-urlpatterns = router.urls
